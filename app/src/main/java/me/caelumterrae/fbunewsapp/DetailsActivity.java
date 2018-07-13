afbdafa931e0f46897742636931440b138d1c9d2
Gavin Lifrieri <gavinlifrieri@fb.com>
2018-07-13 11:09:29 -0700

    initial commit

---
+++ app/src/main/java/me/caelumterrae/fbunewsapp/DetailsActivity.java
@@ -0,0 +1,13 @@
+package me.caelumterrae.fbunewsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
