eecf63395562468e376315ec20419d896fc9046e
Maddie Wang <maddiew@fb.com>
2018-07-24 16:04:36 -0700

    Finished signup + login handlers

--- app/src/main/java/me/caelumterrae/fbunewsapp/handlers/database/LoginHandler.java
+++ app/src/main/java/me/caelumterrae/fbunewsapp/handlers/database/LoginHandler.java
@@ -1,7 +1,9 @@
 package me.caelumterrae.fbunewsapp.handlers.database;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -9,11 +11,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
+import me.caelumterrae.fbunewsapp.activities.LoginActivity;
+import me.caelumterrae.fbunewsapp.activities.SwipeActivity;
 
-public class GetLikeHandler extends JsonHttpResponseHandler {
+
+// This handler gets called in: LoginActivity login button handler
+// This handler: moves user to timeline intent with UID packaged inside
+public class LoginHandler extends JsonHttpResponseHandler {
 
     Context context;
-    public GetLikeHandler(Context context) {
+    public LoginHandler(Context context) {
         this.context = context;
     }
 
@@ -22,10 +29,14 @@ public class GetLikeHandler extends JsonHttpResponseHandler {
         try {
             int UID = response.getInt("UID");
             if (UID != -1) {
-                // start next intent
+                // start next intent to Swipeactivity
+                final Intent intent = new Intent(context, SwipeActivity.class);
+                intent.putExtra("uid", UID);
+                context.startActivity(intent);
             }
             else {
                 // error!!
+                Toast.makeText(context, "Login Error", Toast.LENGTH_LONG);
             }
 
         } catch (JSONException e) {
