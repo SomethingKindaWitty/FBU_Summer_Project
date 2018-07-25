876d242a3c0ce547cdbd96e111243af93fd39f06
Gavin Lifrieri <gavinlifrieri@fb.com>
2018-07-25 16:16:45 -0700

    touch to start intent from the graphics fragment

--- app/src/main/java/me/caelumterrae/fbunewsapp/fragments/GraphicsFragment.java
+++ app/src/main/java/me/caelumterrae/fbunewsapp/fragments/GraphicsFragment.java
@@ -1,6 +1,7 @@
 package me.caelumterrae.fbunewsapp.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,7 +10,12 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.parceler.Parcels;
+
+import me.caelumterrae.fbunewsapp.activities.DetailsActivity;
 import me.caelumterrae.fbunewsapp.graphics.MyGLRenderer;
+import me.caelumterrae.fbunewsapp.model.Post;
+import me.caelumterrae.fbunewsapp.model.User;
 
 
 public class GraphicsFragment extends Fragment {
@@ -62,23 +68,36 @@ public class GraphicsFragment extends Fragment {
                     float dx = x - mPreviousX;
                     float dy = y - mPreviousY;
 
-//                    // reverse direction of rotation above the mid-line
-//                    if (y > getHeight() / 2) {
-//                        dx = dx * -1 ;
-//                    }
-//
-//                    // reverse direction of rotation to left of the mid-line
-//                    if (x < getWidth() / 2) {
-//                        dy = dy * -1 ;
-//                    }
-
-//                    mRenderer.setAngle(
-//                            mRenderer.getAngle() +
-//                                    ((dx + dy) * TOUCH_SCALE_FACTOR));
+    //                    // reverse direction of rotation above the mid-line
+    //                    if (y > getHeight() / 2) {
+    //                        dx = dx * -1 ;
+    //                    }
+    //
+    //                    // reverse direction of rotation to left of the mid-line
+    //                    if (x < getWidth() / 2) {
+    //                        dy = dy * -1 ;
+    //                    }
+    //                    mRenderer.setAngle(
+    //                            mRenderer.getAngle() +
+    //                                    ((dx + dy) * TOUCH_SCALE_FACTOR));
 
                     mRenderer.setMove( mRenderer.getX() + dx* TOUCH_SCALE_FACTOR*0.002f, mRenderer.getY() - dy * TOUCH_SCALE_FACTOR*0.002f);
                     requestRender();
                     mRenderer.translate(dx* TOUCH_SCALE_FACTOR*0.002f, dy * TOUCH_SCALE_FACTOR*0.002f);
+                    break;
+                case MotionEvent.ACTION_DOWN:
+                    Post post = new Post();
+                    post.setUrl("https://www.reuters.com/article/us-facebook-fang/facebooks-disappointing-report-hits-rest-of-fang-idUSKBN1KF2X1");
+                    post.setImageUrl("https://s3.reutersmedia.net/resources/r/?m=02&d=20180725&t=2&i=1287042306&r=LYNXMPEE6O20K&w=1280");
+                    post.setTitle("test");
+                    post.setBody("test");
+                    Intent i = new Intent(getContext(), DetailsActivity.class);
+                    i.putExtra(Post.class.getSimpleName(), Parcels.wrap(post));
+                    //Log.e("User", userID);
+                    i.putExtra(User.class.getSimpleName(), 2);
+                    getContext().startActivity(i);
+                    break;
+
             }
 
             mPreviousX = x;
