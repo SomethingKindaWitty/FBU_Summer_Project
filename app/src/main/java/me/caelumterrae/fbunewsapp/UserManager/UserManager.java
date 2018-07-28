2bd0be2d117d6e357c8d94013802e5da54fcf607
Maddie Wang <maddiew@fb.com>
2018-07-27 18:02:37 -0700

    added more to usermanager

--- app/src/main/java/me/caelumterrae/fbunewsapp/UserManager/UserManager.java
+++ app/src/main/java/me/caelumterrae/fbunewsapp/UserManager/UserManager.java
@@ -1,5 +1,11 @@
 package me.caelumterrae.fbunewsapp.UserManager;
 
+import org.json.JSONException;
+
+import java.util.concurrent.CountDownLatch;
+
+import me.caelumterrae.fbunewsapp.client.ParseNewsClient;
+import me.caelumterrae.fbunewsapp.handlers.database.GetUserHandler;
 import me.caelumterrae.fbunewsapp.model.User;
 
 // Class that manages the current user (a cache'd copy).
@@ -8,12 +14,28 @@ public class UserManager {
 
     private static User myUser;
     private static Object lock = new Object();
+    private static CountDownLatch latch  = new CountDownLatch(1);
+    private ParseNewsClient parseNewsClient;
     // lock prevents us from accessing and modifying the user object at the same time
 
-    public static User getUser() {
+
+    // Creates and populates a user given a UID
+    public UserManager(int uid) throws JSONException {
         synchronized (lock) {
+            myUser = new User();
+            parseNewsClient.getUser(uid, new GetUserHandler(myUser, latch));
+        }
+    }
+
+    public static User getUser() throws InterruptedException {
+        synchronized (lock) {
+            latch.await();
             return myUser;
         }
+    }
+
+    public static void refreshUser() {
+        // network call?
     }
 
     public static void setUser(User user) {
