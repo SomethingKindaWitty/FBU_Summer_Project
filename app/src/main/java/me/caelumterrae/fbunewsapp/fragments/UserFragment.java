ed54d1a8518e822bd5a13410ad4b4c3f38617388
IAFDI <magdalenaaprice@gmail.com>
2018-07-31 17:04:09 -0700

    camera is able to be opened, problem with either response delay or storing url

--- app/src/main/java/me/caelumterrae/fbunewsapp/fragments/UserFragment.java
+++ app/src/main/java/me/caelumterrae/fbunewsapp/fragments/UserFragment.java
@@ -1,13 +1,21 @@
 package me.caelumterrae.fbunewsapp.fragments;
 
 import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.util.SparseIntArray;
@@ -15,10 +23,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
@@ -48,6 +58,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.parceler.Parcels;
 
+import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
@@ -58,10 +69,13 @@ import java.util.concurrent.Semaphore;
 import me.caelumterrae.fbunewsapp.R;
 import me.caelumterrae.fbunewsapp.client.ParseNewsClient;
 import me.caelumterrae.fbunewsapp.handlers.TimelineHandler;
+import me.caelumterrae.fbunewsapp.handlers.database.UserProfileImageHandler;
 import me.caelumterrae.fbunewsapp.math.BetaDis;
 import me.caelumterrae.fbunewsapp.handlers.database.GetUserHandler;
 import me.caelumterrae.fbunewsapp.model.User;
 import me.caelumterrae.fbunewsapp.singleton.CurrentUser;
+
+import static android.app.Activity.RESULT_OK;
 
 public class UserFragment extends Fragment {
 
@@ -71,11 +85,19 @@ public class UserFragment extends Fragment {
     public TextView politicalAffiliation;
     public TextView otherPoliticalAffiliation;
     public TextView numUpvoted;
+    public ImageButton takePicture;
     private SwipeRefreshLayout swipeContainer;
     MediaPlayer mediaPlayer;
 
     public GraphView graph;
     private User user;
+
+    // For taking/storing profile image upon Camera intent
+    public final String APP_TAG = "NewsApp";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 219;
+    public String photoFileName = "photo.jpg";
+    private String imagePath = null;
+    File photoFile;
 
     public UserFragment(){
 
@@ -92,16 +114,25 @@ public class UserFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         swipeContainer = view.findViewById(R.id.swipeContainer);
+        takePicture = view.findViewById(R.id.camera);
 
         user = CurrentUser.getUser();
 
         if (user != null){
-            createUser(view, user.getUsername(), user.getPoliticalPreference(), user.getNumUpvoted());
+            createUser(view, user.getUsername(), user.getPoliticalPreference(), user.getNumUpvoted(), user.getProfileUrl());
             //create our quacking refresh sound
             final View view1 = view;
             //create our quacking refresh sound
             mediaPlayer = MediaPlayer.create(getContext(),R.raw.quack);
             final MediaPlayer quack_sound = mediaPlayer;
+
+            takePicture.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onLaunchCamera();
+                }
+            });
+
 
             swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
                 @Override
@@ -110,7 +141,7 @@ public class UserFragment extends Fragment {
                     // Make sure you call swipeContainer.setRefreshing(false)
                     // once the network request has completed successfully.
                     quack_sound.start();
-                    createUser(view1 ,user.getUsername(), user.getPoliticalPreference(), user.getNumUpvoted());
+                    createUser(view1 ,user.getUsername(), user.getPoliticalPreference(), user.getNumUpvoted(), user.getProfileUrl());
                     swipeContainer.setRefreshing(false);
                 }
             });
@@ -122,7 +153,7 @@ public class UserFragment extends Fragment {
 
     }
 
-    public void createUser(View view, String name, double politicalAff, int numVotes) {
+    public void createUser(View view, String name, double politicalAff, int numVotes, String profileUrl) {
 
         int pol_num = (int) politicalAff;
 
@@ -139,7 +170,6 @@ public class UserFragment extends Fragment {
         politicalAffiliation = view.findViewById(R.id.politicalNum);
         numUpvoted = view.findViewById(R.id.numUpvoted);
         otherPoliticalAffiliation = view.findViewById(R.id.affiliationScore2);
-
 
         username.setText(name);
         numUpvoted.setText(String.valueOf(numVotes));
@@ -165,11 +195,20 @@ public class UserFragment extends Fragment {
             politicalAffiliationWord.setText("Conservative");
         }
 
-        Glide.with(getContext())
-                .load(R.drawable.duckie)
-                .apply(new RequestOptions().fitCenter())
-                .into(profileImage);
-
+        // Checks to see if the user has already set a profile image
+        // and loads appropriate image
+        if (profileUrl.equals("null")) {
+            Glide.with(getContext())
+                    .load(R.drawable.duckie)
+                    .apply(new RequestOptions(). circleCrop())
+                    .into(profileImage);
+        } else {
+            Log.e("URL", profileUrl);
+            Glide.with(getContext())
+                    .load(profileUrl)
+                    .apply(new RequestOptions().circleCrop().error(R.drawable.user))
+                    .into(profileImage);
+        }
 
         // Sets up beta distribution graph ---- TODO: replace affiliation w/ real affiliation - done
         // TODO put this in another function
@@ -272,6 +311,75 @@ public class UserFragment extends Fragment {
         RadarData data = new RadarData(radarDataSets);
         radarChart.setData(data);
         radarChart.invalidate();
+    }
 
+    // Creates the intent to the camera object of the phone and
+    // creates a file path to be utilized later, storing it
+    // and starting the intent
+    public void onLaunchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // Wrap File object into a content provider
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "me.caelumterrae.fbunewsapp", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // Ensures that the phone has an app that can handle the camera intent
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+        imagePath = mediaStorageDir.getPath() + File.separator + fileName;
+        return file;
+    }
+
+    // If the result from the camera intent was successful, loads new profile image
+    // and sets the user
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(takenImage, 246, 150, true);
+                Drawable d = new BitmapDrawable(getResources(), scaledBitmap);
+
+                Glide.with(getContext())
+                        .load(d)
+                        .apply(new RequestOptions().circleCrop())
+                        .into(profileImage);
+
+                user.setProfileUrl(photoFile.getAbsolutePath());
+
+                // Make call to database to update user
+                ParseNewsClient parseNewsClient = new ParseNewsClient(getContext());
+                try {
+                    parseNewsClient.setProfileImage(user.getUid(), user.getProfileUrl(), new UserProfileImageHandler(getContext()));
+                    Log.e("new url", user.getProfileUrl());
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                Toast.makeText(getActivity(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
