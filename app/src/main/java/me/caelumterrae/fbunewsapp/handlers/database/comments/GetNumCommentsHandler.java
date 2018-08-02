045babadb2e81ba8f4d520f56584a765c4b63556
IAFDI <magdalenaaprice@gmail.com>
2018-08-02 14:35:33 -0700

    comments properly show upon refresh

---
+++ app/src/main/java/me/caelumterrae/fbunewsapp/handlers/database/comments/GetNumCommentsHandler.java
@@ -0,0 +1,47 @@
+package me.caelumterrae.fbunewsapp.handlers.database.comments;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+// This handler gets called in: TODO
+// This handler: sets number of comments posted to the user object
+public class GetNumCommentsHandler extends JsonHttpResponseHandler {
+
+    Context context;
+    List<Integer> numComments;
+
+    public GetNumCommentsHandler(Context context, List<Integer> numComments) {
+        this.context = context;
+        this.numComments = numComments;
+    }
+
+    @Override
+    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+        int num = 0;
+        try {
+            num = response.getInt("num");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        numComments.clear();
+        numComments.add(num);
+        Log.e("num after", Integer.toString(numComments.get(0)));
+        Log.e("NumComments", "success");
+    }
+
+    @Override
+    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+        Log.e("NumComments", "failure");
+    }
+
+}
